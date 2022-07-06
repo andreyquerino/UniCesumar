9d3fa76dbbb9d2787ad146455e45a89c9c3b74c4
Andrey <88719652+andreyquerino@users.noreply.github.com>
2022-07-06 11:14:01 -0300

    Update Mapa.c

--- ESTRUTURA-DE-DADOS-I/Mapa.c
+++ ESTRUTURA-DE-DADOS-I/Mapa.c
@@ -8,7 +8,8 @@
 
 #define QUANTIDADE 5 // Quantidade de armazenamento
 
-typedef struct {
+typedef struct
+{
     int RA;           // RA do aluno
     int matricula;    // Matrículacula do professor responsável pela correção
     char nome[20];    // Nome do arquivo
@@ -19,91 +20,109 @@ Atividade fila[QUANTIDADE];
 int opcoes, tamanho;
 
 // Inserir Elemento Na lista
-void inserirAtividade(){
+void inserirAtividade()
+{
     int continuar;
-    do {
+    do
+    {
         system("cls");
         printf(".:: Inserir Nova Atividade ::.\n\n");
-        if (tamanho < QUANTIDADE){
+        if (tamanho < QUANTIDADE)
+        {
             printf("RA do aluno: ");
-            scanf("%d",&fila[tamanho].RA);
+            scanf("%d", &fila[tamanho].RA);
             fflush(stdin);
             printf("Nome do arquivo: ");
-            scanf("%20[^\n]s",&fila[tamanho].nome);
+            scanf("%20[^\n]s", &fila[tamanho].nome);
             fflush(stdin);
             printf("ExtensÃ£o do arquivo: ");
-            scanf("%5[^\n]s",&fila[tamanho].extensao);
+            scanf("%5[^\n]s", &fila[tamanho].extensao);
             fflush(stdin);
-            printf("Matrículacula do professor responsável pela correção: ");
-            scanf("%d",&fila[tamanho].matricula);
+            printf("Matrícula do professor responsável pela correção: ");
+            scanf("%d", &fila[tamanho].matricula);
             fflush(stdin);
             tamanho++;
             printf("\nAtividade inserida com sucesso!\n\n");
         }
-        else {
+        else
+        {
             printf("\n\nFila Cheia!\n\n");
             system("pause");
         }
         printf("\nDeseja Continuar Inserindo [1]SIM / [0]NÃO)? ");
-        scanf("%d",&continuar);
-        if (continuar > 1){
+        scanf("%d", &continuar);
+        if (continuar > 1)
+        {
             printf("\nOpção Inválida!\n");
             system("pause");
         }
-    }while (continuar == 1);
+    } while (continuar == 1);
 }
 
 // Remove o último elemento da fila
-void RemoverUmaAtividade(){
-    if (tamanho != 0){
+void RemoverUmaAtividade()
+{
+    if (tamanho != 0)
+    {
         fila[tamanho--];
         printf("\n\nRemovido uma das atividade com sucesso!\n\n");
         system("pause");
     }
-    else {
-        system("cls");       
+    else
+    {
+        system("cls");
         printf("\n\nFila Vazia!\n");
         system("pause");
     }
 }
 
 // Esvazia toda a fila
-void EsvaziarFila(){
-    int i;
-    if (QUANTIDADE == 0){
-        system("cls");
-        printf("A fila já está vazia!");
-        system("pause");
-    }
-    else {
-        for (i = 0; i <= QUANTIDADE; i++){
+void EsvaziarFila()
+{
+    int j;
+    if (tamanho != 0)
+    {
+        for (j = 0; j <= tamanho; j++)
+        {
             fila[tamanho--];
         }
+    }
+    else
+    {
+        system("cls");
+        printf("A fila já está vazia!\n\n");
+        system("pause");
     }
 }
 
 // Mostra toda a fila
-void ListarAtividades(){
+void ListarAtividades()
+{
     int i;
     system("cls");
     printf(".:: Lista de Atividades ::.\n\n");
-    if ( tamanho != 0){
-        for (i = 0; i < tamanho; i++){
-            printf("RA do aluno: %d\n",fila[i].RA);
-            printf("Nome do arquivo: %s\n",fila[i].nome);
-            printf("Extensão do arquivo: %s\n",fila[i].extensao);
-            printf("Matrículacula do professor responsável pela correção: %d\n\n",fila[i].matricula);     
+    if (tamanho != 0)
+    {
+        for (i = 0; i < tamanho; i++)
+        {
+            printf("RA do aluno: %d\n", fila[i].RA);
+            printf("Nome do arquivo: %s\n", fila[i].nome);
+            printf("Extensão do arquivo: %s\n", fila[i].extensao);
+            printf("Matrícula do professor responsável pela correção: %d\n\n", fila[i].matricula);
         }
     }
-    else{
+    else
+    {
         printf("\nNenhuma Atividade na Fila!\n\n");
     }
 }
 
-int main(){
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
     tamanho = 0;
-    do {
+    do
+    {
         ListarAtividades();
         printf(".:: MENU DE ATIVIDADES DOS ALUNOS ::.\n");
         printf("[1] Inserir uma atividade na fila\n");
@@ -112,13 +131,22 @@ int main(){
         printf("[0] Encerrar o programa\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcoes);
-        switch(opcoes){
-            case 1: inserirAtividade(); break;
-            case 2: RemoverUmaAtividade(); break;
-            case 3: EsvaziarFila(); break;
-            case 0: break;
-            default: printf("\nOpção Inválida!");
-            system("pause");
+        fflush(stdin);
+        switch (opcoes)
+        {
+        case 1:
+            inserirAtividade();
+            break;
+        case 2:
+            RemoverUmaAtividade();
+            break;
+        case 3:
+            EsvaziarFila();
+            break;
+        case 0:
+            break;
+        default:
+            printf("\nOpção Inválida!");
         }
     } while (opcoes != 0);
 }
